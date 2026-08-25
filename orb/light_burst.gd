f4b2fb0d4d8fa682aa82220f36d6a8f49070a60c
DHA343 <lkjsdf343@gmail.com>
2026-08-25 20:00:12 +0900

    5

--- orb/light_burst.gd
+++ orb/light_burst.gd
@@ -1,6 +1,8 @@
 @tool
 extends GPUParticles2D
 
+const TRAIL_WIDTH_SCALE := 0.12
+
 @export_range(1.0, 1024.0, 1.0) var particle_size: float = 128.0:
 	set(value):
 		particle_size = value
@@ -13,17 +15,36 @@ extends GPUParticles2D
 		if is_inside_tree():
 			_apply_sphere_radius()
 
+@onready var _trail: GPUParticles2D = $Trail
+
 
 func _enter_tree() -> void:
 	_apply_particle_size()
 	_apply_sphere_radius()
 
 
+func _ready() -> void:
+	_synchronize_trail()
+	_apply_particle_size()
+	_apply_sphere_radius()
+
+
 func _apply_particle_size() -> void:
 	set_instance_shader_parameter("particle_size", particle_size)
+	if is_node_ready():
+		_trail.set_instance_shader_parameter("trail_width", particle_size * TRAIL_WIDTH_SCALE)
 
 
 func _apply_sphere_radius() -> void:
 	var shader_material := process_material as ShaderMaterial
 	assert(shader_material != null, "LightBurstにはShaderMaterialを設定してください。")
 	shader_material.set_shader_parameter("sphere_radius", sphere_radius)
+
+
+func _synchronize_trail() -> void:
+	_trail.amount = amount
+	_trail.lifetime = lifetime
+	_trail.local_coords = local_coords
+	_trail.process_material = process_material
+	_trail.use_fixed_seed = true
+	_trail.seed = seed
