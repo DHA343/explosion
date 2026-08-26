8db61c87617327c43a7d9cc38293f40bd4285c83
DHA343 <lkjsdf343@gmail.com>
2026-08-26 11:44:08 +0900

    12

--- orb/burst/light_burst.gd
+++ orb/burst/light_burst.gd
@@ -1,29 +1,29 @@
 @tool
 extends GPUParticles2D
 
-@export_range(1.0, 1024.0, 1.0) var particle_size: float = 128.0:
+@export_range(1.0, 2000.0, 1.0, "suffix:px") var sphere_size: float = 400.0:
 	set(value):
-		particle_size = value
-		if is_inside_tree():
-			_apply_particle_size()
+		sphere_size = value
+		if process_material != null:
+			_apply_process_parameters()
 
-@export_range(1.0, 1000.0, 1.0) var sphere_radius: float = 450.0:
+
+@export_range(1.0, 200.0, 1.0, "suffix:px") var particle_width: float = 140.0:
 	set(value):
-		sphere_radius = value
-		if is_inside_tree():
-			_apply_sphere_radius()
+		particle_width = value
+		if process_material != null:
+			_apply_process_parameters()
 
 
 func _enter_tree() -> void:
-	_apply_particle_size()
-	_apply_sphere_radius()
+	if not Engine.is_editor_hint():
+		use_fixed_seed = true
+		seed = randi()
+	_apply_process_parameters()
 
 
-func _apply_particle_size() -> void:
-	set_instance_shader_parameter("particle_size", particle_size)
-
-
-func _apply_sphere_radius() -> void:
+func _apply_process_parameters() -> void:
 	var shader_material := process_material as ShaderMaterial
 	assert(shader_material != null, "LightBurstにはShaderMaterialを設定してください。")
-	shader_material.set_shader_parameter("sphere_radius", sphere_radius)
+	shader_material.set_shader_parameter("radius", sphere_size)
+	shader_material.set_shader_parameter("particle_width", particle_width)
